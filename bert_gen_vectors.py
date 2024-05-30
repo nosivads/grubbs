46a5287d76988e8f4fc40a59ac47970f7b1358d0
Stephen Davison <sdavison@caltech.edu>
2024-05-30 14:36:55 -0700

    Update embeddings creation

--- bert_gen_vectors.py
+++ bert_gen_vectors.py
@@ -3,18 +3,18 @@ from transformers import pipeline
 
 import pandas as pd
 import numpy as np
-
+'''
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import euclidean_distances
-
+'''
 import sys, os
 from pathlib import Path
 
 from spire.doc import *
 from spire.doc.common import *
-
-import eml_parser, datetime, json
-
+'''
+import datetime, json
+'''
 try:
     top=Path(sys.argv[1])
 except:
@@ -22,23 +22,18 @@ except:
 try:
     out=Path(sys.argv[2])
 except:
-    out='embeddings.csv'
-
-# for email parsing
-def json_serial(obj):
-    if isinstance(obj, datetime.datetime):
-        serial = obj.isoformat()
-        return serial
+    out=Path('embeddings.csv')
 
 bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
 
-df = pd.DataFrame(columns=['folder', 
+embeddings_df = pd.DataFrame(columns=['folder', 
                            'filename',
-                           'length',
-                           'embedding'])
+                           'text',
+                           'length'])
 
 i = 0
 for path in top.rglob('*'):
+
     if path.suffix == '.txt':
         text = path.read_text()
     elif path.suffix == '.doc' or path.suffix == '.docx':
@@ -46,27 +41,24 @@ for path in top.rglob('*'):
         document = Document()
         document.LoadFromFile(spath)
         text = document.GetText().replace('Evaluation Warning: The document was created with Spire.Doc for Python.', '')
-    elif path.suffix == '.eml':
-        with open(path, 'rb') as f:
-            raw_email = f.read()
-            print(raw_email)
-        ep = eml_parser.EmlParser()
-        parsed_eml = ep.decode_email_bytes(raw_email)
-        text = json.dumps(parsed_eml, default=json_serial)
     else:
         continue
-    embedding = bert_model.encode(text)
+
     print('processing...', path.parent, path.name)
     i += 1
-    df.loc[len(df)] = [path.parent,
-                       path.name,
-                       len(text),
-                       embedding]
+    embeddings_df.loc[len(embeddings_df)] = [path.parent,
+                                             path.name,
+                                             text,
+                                             len(text)]
+
+print('creating embeddings...')
+embeddings_df['embedding'] = embeddings_df.text.apply(lambda x: bert_model.encode(x))
     
-df.to_csv(out)
+print('writing CSV...')
+embeddings_df.to_csv(out)
 
 print('number of files:', i)
-print('output written to:', out)
+print('output:', out)
         
                            
 
